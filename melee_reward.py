441c79a393aa27a06e038971f2424bf77ac5d277
NicholasCartaxo <nicholas.cartaxo.de.souza.rangel.alves@ccc.ufcg.edu.br>
2026-07-12 04:38:11 -0300

    fix: reward function

--- melee_reward.py
+++ melee_reward.py
@@ -21,18 +21,28 @@ def calculate_reward(prev_gs: melee.GameState, curr_gs: melee.GameState, port: i
   if player_lost_stock:
     reward -= 1.0
 
-  player_went_offstage = player.off_stage and not prev_player.off_stage
-  enemy_went_offstage = enemy.off_stage and not prev_enemy.off_stage
+  l_bound = melee.BLASTZONES[curr_gs.stage][0]
   
-  if enemy_went_offstage:
-    reward += 0.25
+  player_dead = player.action.value <= 0x0d
+  enemy_dead = enemy.action.value <= 0x0d
 
-  if player_went_offstage:
-    reward -= 0.25
+  if not player_dead:
+    player_dist = abs(player.position.x/l_bound)
+    reward -= 0.0005 * player_dist
+
+    if player.off_stage:
+      reward -= 0.0005
+  
+  if not enemy_dead:
+    enemy_dist = abs(enemy.position.x/l_bound)
+    reward += 0.0005 * enemy_dist
+
+    if enemy.off_stage:
+      reward += 0.0005
   
   damage_dealt = 0.0 if enemy_lost_stock else max(0.0, enemy.percent - prev_enemy.percent)
   damage_taken = 0.0 if player_lost_stock else max(0.0, player.percent - prev_player.percent)
 
-  reward += 0.005 * (damage_dealt - damage_taken)
+  reward += 0.01 * (damage_dealt - damage_taken)
 
   return reward
